ee7cf702532a213d0f6cbe8b8b233ef61f48f795
Ryan Stanton <stantor195@haystack.(none)>
2012-10-22 08:54:30 -0400

    Added a few tests for game board

--- Quoridor/testing/GameObjects_test/TestBoard.java
+++ Quoridor/testing/GameObjects_test/TestBoard.java
@@ -1,4 +1,4 @@
-package GameObject_test;
+package GameObjects_test;
 
 import static org.junit.Assert.*;
 
@@ -34,7 +34,7 @@ public class TestBoard {
 			for (int j = 0; j < 9; j++) {
 				Coordinates curCoord = new Coordinates(i, j);
 				board.setUnoccupied(curCoord);
-				assertTrue(board.isOccupied(i, j));
+				assertTrue(!board.isOccupied(i, j));
 			}
 		}
 	}
@@ -56,4 +56,37 @@ public class TestBoard {
 	}
 	
 	
+
+	
+	@Test
+	public void testhasBottomWall() {
+		
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				board.setHorizontalWall(i, j);
+				board.hasBottomWall(i, j);
+				//board.getTile(i, j).getBottomWall().
+				assertTrue(board.hasBottomWall(i, j));
+				
+			}
+		}
+		
+		
+	}
+	
+	@Test
+	public void testhasRightWall() {
+		
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				board.setVerticalWall(i, j);
+				
+				//board.getTile(i, j).getBottomWall().
+				assertTrue(board.hasRightWall(i, j));
+				
+			}
+		}
+		
+		
+	}
 }
