fcaeca3509ddbe4b7f463a6ee39ee71365762065
Bill Kline <klinewa193@potsdam.edu>
2012-10-25 17:20:37 -0400

    Added start of testing for move server. -- Bill Kline

---
+++ Quoridor/testing/GUI_test/MoveServerTest.java
@@ -0,0 +1,30 @@
+package GUI_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoveServerTest {
+
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSetName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStartServer() {
+        fail("Not yet implemented");
+    }
+
+}
